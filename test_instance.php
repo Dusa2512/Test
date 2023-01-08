aa0e74c619bce5cb6cf3fb9458a521e381634e43
sang <sangdd2512cv@gmail.com>
2023-01-08 23:16:30 +0700

    file knowleage new

--- test_instance.php
+++ test_instance.php
@@ -1,29 +1,42 @@
 <?php
 
-class Person{
+class Person
+{
 
-      public $name;
-      // public $age;
-      // public $gender;
+    private $name;
+    public $age;
 
-      public function __construct($name){
-            $this->name=$name ;
-           
-      }
+    // public $gender;
 
-      // function set_age($age){
-      //       $this->age ;
-           
-      // }
+    public function __construct($name)
+    {
+        $this->name = $name;
 
-      // function set_gender($gender){
-      //       $this->gender;
-           
-      // }
+    }
+
+    public function getName(){
+          return $this->name;
+    }
+
+    function set_age($age)
+    {
+        $this->age = $age;
+
+    }
+
+    // function set_gender($gender){
+    //       $this->gender;
+
+    // }
+}
 
 
-      }
-      $person = new Person('sang');
 
-      echo $person->name;
+
+$person = new Person('sang', 20);
+
+echo $person->getName();
+
+echo $person->age;
+var_dump($person);
 ?>
