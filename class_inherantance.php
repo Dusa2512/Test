8cd013138c1ae4fdc9200d1ed958f6b00d2085d3
sang <sangdd2512cv@gmail.com>
2023-01-11 21:36:22 +0700

    new section

--- class_inherantance.php
+++
@@ -1,9 +0,0 @@
-<?php
-$str1 ='training programming';
-$str2 ='PHP';
-
-echo $str1.$str2.'<br/>';
-?>
-<a href="dusa.net"><img src="https://www.invert.vn/media/downloads/221203T1358_228.jpg" /> </a>
-
-<?php
